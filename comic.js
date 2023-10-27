8693db141da6ea6cc19a33f8e0e70a4233bb6209
GIOVANNA SILVA BELO <23111786@CFP127ALUNO.LOCAL>
2023-10-27 15:36:31 -0300

    fazendo

--- comic.js
+++ comic.js
@@ -1,17 +1,21 @@
 'use strict'
 
+
 async function getComics(){
     const endPoint = "http://ponyapi.net/v1/comics-story/all"
     const response = await fetch(endPoint)
     const comics = await response.json()
+
+    console.log(comics)
     return comics.data
+
+
 }
 
-
 async function loadComics(){
-
 
     const comics = await getComics()
     console.log(comics)
 }
 loadComics()
+getComics()
