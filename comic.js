ecb5e74ab20ecf15a7053b6745fe34dcc6521b8a
Giovanna Belo <123781727+giovbel@users.noreply.github.com>
2023-11-01 08:29:02 -0300

    fazendo

--- comic.js
+++ comic.js
@@ -12,10 +12,82 @@ async function getComics(){
 
 }
 
+
+const criarCards = function(comic){
+    const comicsContainer = document.getElementById('comics')
+
+    const card = document.createElement("div")
+    card.classList.add("storys")
+    
+
+    const cardStory = document.createElement('div')
+    cardStory.classList.add("card-story")
+
+    const imgCapa = document.createElement('img')
+    cardStory.appendChild(imgCapa)
+    imgCapa.src = comic.image
+
+
+    const titleComic = document.createElement('p')
+    titleComic.textContent = comic.name
+
+    const idComic = document.createElement('h3')
+    idComic.textContent = comic.id
+    
+    
+    card.replaceChildren(cardStory, titleComic, idComic)
+    comicsContainer.appendChild(card)
+
+    cardStory.addEventListener ('click', mostrarInfoComics(comic))
+}
+
+const mostrarInfoComics = function(comic){
+
+    const infoComic = document.getElementById('storysComic')
+
+    const sumir = document.createElement('div')
+    sumir.classList.remove("sumir")
+
+    const imageId = document.createElement('div')
+    imageId.classList.add("imgEid")
+    
+
+    const imageCapa = document.createElement('img')
+    imageCapa.src = comic.image
+
+    const idStory = document.createElement('h4')
+    idStory.textContent = comic.id
+
+    const infoCard = document.createElement('div')
+    infoCard.classList.add("info")
+
+    const nameComic = document.createElement('h1')
+    nameComic.textContent = comic.name
+
+    const serieComic = document.createElement('p')
+    serieComic.textContent = comic.series
+
+    const infoStory = document.createElement('p')
+    infoStory.textContent = `author: ${comic.writer}  artist: ${comic.artist} colorist: ${comic.colorist} letterer: ${comic.letterer} editor: ${comic.editor} issue: ${comic.issue}`
+    
+
+    imageId.replaceChildren(imageCapa, idStory)
+    infoCard.replaceChildren(nameComic, serieComic, infoStory)
+    infoComic.replaceChildren(imageId, infoCard)
+
+}
+
 async function loadComics(){
 
     const comics = await getComics()
+    comics.forEach(criarCards)
+
     console.log(comics)
+
+
 }
 loadComics()
 getComics()
+
+//------------------------------------------------------------------
+
